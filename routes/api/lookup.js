8e37f70c174b31613916f505443fd261afa37019
ROBERT P CAREY III <rpc5102@psu.edu>
2020-02-18 10:30:32 -0500

    Restored basic api functionality

--- routes/api/lookup.js
+++ routes/api/lookup.js
@@ -5,42 +5,72 @@ const router = express.Router();
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
-const wordpos = require('wordpos');
+
+const dictionary_en_us = path.dirname(require.resolve('dictionary-en-us'));
+const dictionary = fs.readFileSync(path.join(dictionary_en_us, 'index.dic'));
+const affix = fs.readFileSync(path.join(dictionary_en_us, 'index.aff'));
+
+const Nodehun = require('nodehun'),
+      nodehun = new Nodehun(affix, dictionary);
+
 const Filter = require('bad-words'),
       filter = new Filter();
 
-const dictionary_en_us = path.dirname(require.resolve('dictionary-en-us'));
+const syllable = require('syllable');
 
-const Nodehun = require('nodehun');
+/* * Useful for checking pronunciations * *
+    const CMUDict = require('cmudict').CMUDict,
+    cmudict = new CMUDict();
+    pronunciation = cmudict.get('example');
+    @return {string} 'IH0 G Z AE1 M P AH0 L'
+*/
 
 router.get('/*', (req, res) => {
-    const querying = Object.keys(req.query).length !== 0;
+  const querying = Object.keys(req.query).length !== 0;
 
-    const dictionary = fs.readFileSync(path.join(dictionary_en_us, 'index.dic'));
-    const affix = fs.readFileSync(path.join(dictionary_en_us, 'index.aff'));
-    const nodehun = new Nodehun(affix, dictionary);
-    // const wordnet = new wordpos();
+  if (querying) {
+    if (req.params[0] == 'word') {
+      const terms = req.query.terms.split('-');
 
-    if (querying) {
+      let obj = {};
 
-        if (req.params[0] == 'word') {
-            const terms = req.query.terms.split('-');
-            const results = terms.map(term => {
-                //return wordnet.lookup(term);
-                return nodehun.spell(term);
-            });
+      const results = terms.map(term => {
+        let spelling = nodehun.spell(term),
+            profane = filter.isProfane(term),
+            syllables = syllable(term);
 
-            Promise.all(results).then(results => {
-                results.map(result => {
-                    console.log(result);
+        return Promise.all([spelling, profane, syllables]);
+      });
 
-                })
-                res.json(results);
-            });
+      Promise.all(results).then(results => {
+        for (const key in results) {
+          let word = results[key];
+              wordid = results[key][0],
+              foul = results[key][1],
+              syllables = results[key][2];
+
+          if (word) {
+            if(!wordid) {
+              const CMUDict = require('cmudict').CMUDict,
+                    cmudict = new CMUDict();
+                    pronunciation = cmudict.get(word);
+              wordid = pronunciation.length > 0;
+            }
+            obj[terms[key]] = {
+              'wordid': wordid,
+              'foul': foul,
+              'syllables': syllables 
+            }
+          } else {
+            obj[terms[key]] = false;
+          }
         }
-    } else {
-        res.sendStatus(400);
+        res.json(obj);
+      });
     }
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 module.exports = router;
